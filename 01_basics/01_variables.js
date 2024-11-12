36e75b6a000ebfa280d0eb11d994704d0c09c6eb
NamanT07 <149684481+NamanT07@users.noreply.github.com>
2024-11-12 03:41:43 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+"use strict";//treat all JS code as newer version
+
+// alert("hello")//we are using nodejs,not browser
+
+console.log(3
+    +
+    3)//code readability should be high 
+console.log("Naman")
+
+let name="Naman"
+let age=18
+let isloggedIn=false
+
+//number => 2 to power 53
+//string => ""
+//bigint
+//boolean = true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof "hitest")
+console.log(typeof age)
+console.log(typeof null)//object
+console.log(typeof undefined)//undefine
+
+
