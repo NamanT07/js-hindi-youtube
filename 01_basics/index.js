ebd2df63e5898cb1146f233d391dfdbed5d71926
NamanT07 <149684481+NamanT07@users.noreply.github.com>
2025-04-02 11:38:26 +0000

    loops

--- 01_basics/index.js
+++ 01_basics/index.js
@@ -36,17 +36,52 @@ console.log('Hello mate');
 
 //contructor function
 
-function Rectangle()
-{
-    this.length=1;
-    this.breath=2;
-    this.draw= function()
-    {
-        console.log('Drawing');
-    }
-}
-
-let rectangleObj=new Rectangle();
 
 
- 
+// function Rectangle(len, bre)
+// {
+//     this.length=1;
+//     this.breath=2;
+//     this.draw= function()
+//     {
+//         console.log('Drawing');
+//     }
+// }
+
+
+// let rectangleObj=new Rectangle(4,6);
+
+// rectangleObj.color='yellow';//Dynamic Nature of object
+
+// console.log(rectangleObj);
+
+// delete rectangleObj.color;//Dynamic nature of object
+
+// console.log(rectangleObj);
+
+//In primitives type copy is made thus answer is 11 and 10 in the below example
+
+// let a=10;
+// let b=a;
+// a++;
+// console.log(a);
+// console.log(b);
+
+//while Dealing with objects(ex:array,object) address is [passed] thus a and b both are pointing at same address block
+
+// let a={value:10};
+// let b=a;
+// a.value++;
+
+// console.log(a.value);
+// console.log(b.value);
+
+
+//example of pass by
+let a=10;
+function inc(a){
+    a++;
+};
+inc(a);
+console.log(a);
+//output is 10 because a copy of a is sent to inc function
