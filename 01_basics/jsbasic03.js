49e211db8cd058fea2546343c722ba3626f67056
NamanT07 <149684481+NamanT07@users.noreply.github.com>
2025-04-03 13:09:01 +0000

    arrays

---
+++ 01_basics/jsbasic03.js
@@ -0,0 +1,20 @@
+let numbers=[1,4,5,7];
+
+console.log(numbers);
+
+console.log(numbers);
+
+console.log(numbers.indexOf(9));
+
+//we want to check if a number exist exist in a arraY
+// if(numbers.indexOf(4) != -1)
+//     console.log("present");
+
+// console.log(numbers.includes(7));
+
+// console.log(numbers.indexOf(4,2));
+
+let course=
+{
+    
+}
