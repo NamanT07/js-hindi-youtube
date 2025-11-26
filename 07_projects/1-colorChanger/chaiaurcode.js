cfedb7eeeb199721faac62d281ab884c745346b6
NamanT07 <149684481+NamanT07@users.noreply.github.com>
2025-11-26 12:21:19 +0000

    colorChanger

---
+++ 07_projects/1-colorChanger/chaiaurcode.js
@@ -0,0 +1,30 @@
+const buttons=document.querySelectorAll('.button');
+const body=document.querySelectorAll('body');
+console.log(buttons);
+
+console.log(body);
+
+buttons.forEach((function(button){
+    button.addEventListener('click',function(e){
+        // console.log(e)
+        console.log(e.target)
+        switch (e.target.id) {
+            case "grey":
+                body[0].style.backgroundColor="grey"
+                break;
+        
+            case "yellow":
+                body[0].style.backgroundColor="yellow"
+                break;
+        
+            case "blue":
+                body[0].style.backgroundColor="blue"
+                break;
+        
+            case "white":
+                body[0].style.backgroundColor="white"
+                break;
+
+        }
+    })
+}))
