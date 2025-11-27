19853a74bde14d19843147ea903d7ead799964c4
NamanT07 <149684481+NamanT07@users.noreply.github.com>
2025-11-27 11:46:04 +0000

    guessTheNumber

---
+++ 07_projects/GuessTHeNumber/chaiaurcode.js
@@ -0,0 +1,8 @@
+const randomNumber=parseInt((Math.random()*100 + 1));
+
+const submit=document.querySelector('#subt')
+const userInput=document.querySelector('#guessField')
+const guessSlot=document.querySelector('.gusses')
+const remaining=document.querySelector('.lastResult')
+
+
